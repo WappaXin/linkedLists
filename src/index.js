84d8f0a22ddb6bc5189ddbada7df5e2715daf68e
WappaXin <pradyumnavasudevjoshi@gmail.com>
2025-03-28 14:56:00 +0530

    Create a linked list with edit methods

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import {LinkedList} from "./linkedList.js"
+
+
+// first case
+const list = new LinkedList();
+
+list.append(1);
+list.append(2);
+list.append(3);
+list.append(4);
+list.prepend(0);
+list.head();
+list.tail();
+list.pop();
+list.append(4);
+list.insertAt(2.1 , 5);
+list.removeAt(3);
+list.removeNodeContainingThisValue(1);
+list.at(8);
+
+console.log("");
+console.log("");
+console.log("");
+console.log(list.displayLinkedList());
+console.log(`Number of nodes: ${list.size()}`);
+console.log(`Head-node ${list.head()}`);
+console.log(`Tail-node ${list.tail()}`);
+console.log(list.head());
+console.log(list.tail());
+console.log(`node at index ${JSON.stringify(list.at(2))}`);
+console.log(list.at(2));
+console.log(`Does the list contain the value: ${list.contains(6)}`);
+console.log(list.contains(6));
+console.log(list.findIndexOf(2));
+console.log(`Return the index of the value in the list if present: ${list.findIndexOf(2)}`);
+console.log(list.toString());
+console.log(Array.isArray(list));
+console.log(list);
+console.log(list.findIndexOf(list.tail().value));
+console.log(list.tail().value);
+
+
+// second case
+const list1 = new LinkedList();
+list1.append("dog");
+list1.append("cat");
+list1.append("parrot");
+list1.append("hamster");
+list1.append("snake");
+list1.append("turtle");
+
+console.log("");
+console.log("");
+console.log("");
+console.log("");
+console.log(list1.displayLinkedList())
+console.log(list1.toString());
---
+++ src/linkedList.js
@@ -0,0 +1,275 @@
+import {Node} from "./node.js"
+
+export class LinkedList{
+    constructor(arr = []){
+        this.arr = arr;
+    }
+
+    append(value){
+        if(this.arr[0] === undefined){
+            this.arr[0] = new Node(value);
+            return;
+        }
+        
+        if(this.arr[0] !== undefined){
+            step(this.arr[0]);
+            return;
+        }
+
+        function step(object){
+            if(object.nextNode === null){
+                object.nextNode = new Node(value);
+                return;
+            }
+
+            if(object.nextNode !== null){
+                return step(object.nextNode);
+            }
+        }
+    }
+
+    prepend(value){
+        if(this.arr[0] === undefined){
+            this.arr[0] = new Node(value);
+            return;
+        }
+
+        if(this.arr[0] !== undefined){
+            this.arr[0] = new Node(value , this.arr[0]);
+            return;
+        }
+    }
+
+    size(){
+        if(this.arr[0] === undefined){
+            return 0;
+        }
+
+        if(this.arr[0] !== undefined){
+            return step(this.arr[0]);
+        }
+
+        function step(object){
+            if(object.nextNode === null){
+                return 1;
+            }
+
+            if(object.nextNode !== null){
+                return 1 + step(object.nextNode);
+            }
+        }
+    }
+
+    head(){
+        if(this.arr[0] === undefined){
+           return "No node";
+        }
+
+        if(this.arr[0] !== undefined){
+           return this.arr[0];
+        }
+    }
+
+
+    tail(){
+        if(this.arr[0] === undefined){
+           return "No node";
+        }
+
+        if(this.arr[0] !== undefined){
+           return step(this.arr[0]);
+        }
+
+        function step(object){
+            if(object.nextNode === null){
+                return object;
+            }
+
+            if(object.nextNode !== null){
+                return step(object.nextNode);
+            }
+        }
+
+    }
+
+    at(index){
+        let lastNode = this.findIndexOf(this.tail().value);
+        if(this.arr[0] === undefined){
+            return "empty linkedList";
+        }
+
+        if(this.arr[0] !== undefined){
+            if(index > lastNode || index < 0) return;
+            return step(this.arr[0]);
+        }
+
+        function step(object , index1 = 0){
+            if(index1 === index){
+                return object;
+            }
+
+            if(index1 !== index){
+                return step(object.nextNode , index1 + 1);
+            }
+        }
+    }
+
+    pop(){
+        if(this.arr[0] === undefined){
+            return "empty linkedList";
+        //if there is only one element in the linked list then linkedList will be empty 
+        }else if(this.arr[0].nextNode === null){
+            this.arr = [];
+        }
+
+        // if linkedList contains more than 1
+        if(this.arr[0].nextNode !== null){
+            return step(this.arr[0]);
+        }
+
+        function step(object){
+            if(object.nextNode.nextNode === null){
+                object.nextNode = null; 
+                return;
+            }
+
+            if(object.nextNode.nextNode !== null){
+                return step(object.nextNode);
+            }
+        }
+    }
+
+    contains(value){
+        if(this.arr[0] === undefined){
+            return false;
+        }
+
+        if(this.arr[0] !== undefined){
+            return step(this.arr[0]);
+        }
+
+        function step(object){
+            if(object.value === value){
+                return true;
+            }else if(object.nextNode === null && object.value !== value){
+                return false;
+            }
+
+            if(object.value !== value){
+                return step(object.nextNode);
+            }
+
+        }
+    }
+
+    findIndexOf(value){
+        if(this.arr[0] === undefined){
+            return null;
+        }
+
+        if(this.arr[0] !== undefined){
+            let index1 = step(this.arr[0]);
+
+            if(isNaN(index1)){
+                return null;
+            }
+
+            return index1;
+        }
+
+        function step(object , index2 = 0){
+            if(object.value === value){
+                // since we are adding 1 every time object doesn't contain given value,
+                // If value is found then returning 0 would make the total index value-
+                // -as if index starts from 0
+                return 0;
+            }else if( object.nextNode === null && object.value !== value ){
+                return NaN;
+            }
+
+            if(object.value !== value){
+                return 1 + step(object.nextNode , index2 + 1);
+            }
+        }
+    }
+
+    toString(){
+        if(this.arr[0] === undefined){
+            return "Empty linked list";
+        }
+
+        if(this.arr[0] !== undefined){
+            return step(this.arr[0]);
+        }
+
+        function step(object){
+            if(object.nextNode === null){
+                return ` ( ${object.value} ) -> null ` ;
+            }
+
+            if(object.nextNode !== null){
+                return `( ${object.value} ) ->` + step(object.nextNode);
+            }
+        }
+    }
+    
+    
+    insertAt(value , index){
+        let lastNode = this.findIndexOf(this.tail().value);
+        if(this.arr[0] === undefined){
+            return `Empty linked list`;
+        }
+
+        if(this.arr[0] !== undefined){
+            if(index === 0){
+                this.prepend(value);
+                return;
+            // if index value is right after the last node then append it 
+            }else if(index === lastNode + 1){
+                this.append(value);
+                return;
+            }else if( index > lastNode + 1 || index < 0){
+                return;
+            }
+
+            this.at(index - 1).nextNode = new Node(value , this.at(index));
+            return;
+        }
+    }
+
+    removeAt(index){
+        let lastNode = this.findIndexOf(this.tail().value);
+        if(this.arr[0] === undefined){
+            return `Can't remove because it's an empty linked list`;
+        }
+
+        if(this.arr[0] !== undefined){
+            if(index === 0){
+                this.arr[0] = this.at(1);
+                return;
+            }else if(index === lastNode){
+                this.pop();
+                return;
+            }else if(index > lastNode || index < 0){
+                return;
+            }
+
+            this.at(index - 1).nextNode = this.at(index + 1);
+        }
+    }
+
+    removeNodeContainingThisValue(value){
+        // indexOfValue returns null if value not found
+        let indexOfValue = this.findIndexOf(value); 
+        if( indexOfValue === null ){
+            return;
+        }
+
+        this.removeAt(indexOfValue);
+    }
+
+    displayLinkedList(){
+        return this.arr[0];
+    }
+
+}
